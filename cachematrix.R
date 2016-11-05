1f241abd8dd785d3223948c058f688fd40d22f9f
LiYimin <liyimintt@gmail.com>
2016-11-05 12:40:52 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-#
+## <<- rebinds an existing name in a parent of the current environment
         inv <- NULL
     set <- function(matrix) {
         x <<- y
